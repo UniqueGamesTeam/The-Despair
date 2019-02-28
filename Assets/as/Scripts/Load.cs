6f3db6a5de44561aa7f6ec213ba279378887d664
Geksogod <41061522+Geksogod@users.noreply.github.com>
2019-02-28 14:15:10 +0200

    Crash_Car

--- Assets/as/Scripts/Load.cs
+++ Assets/as/Scripts/Load.cs
@@ -10,55 +10,55 @@ public class Load : MonoBehaviour {
     Animator Zombie_dead;
     public void Start()
     {
-        Transform Cur_play_position = this.gameObject.transform;
-        Vector3 Player_Position = new Vector3(PlayerPrefs.GetFloat("PosX"), PlayerPrefs.GetFloat("PosY"), PlayerPrefs.GetFloat("PosZ"));
-        bulletsScipt.BulletsInClip = PlayerPrefs.GetInt("BulletsInClip");
-        bulletsScipt.BulletsInClip = PlayerPrefs.GetInt("BulletsInClip");
-        Cur_play_position.position = Player_Position;
-        save.num = PlayerPrefs.GetInt("Num");
-        save.index = PlayerPrefs.GetInt("Index_");
+        //Transform Cur_play_position = this.gameObject.transform;
+        //Vector3 Player_Position = new Vector3(PlayerPrefs.GetFloat("PosX"), PlayerPrefs.GetFloat("PosY"), PlayerPrefs.GetFloat("PosZ"));
+        //bulletsScipt.BulletsInClip = PlayerPrefs.GetInt("BulletsInClip");
+        //bulletsScipt.BulletsInClip = PlayerPrefs.GetInt("BulletsInClip");
+        //Cur_play_position.position = Player_Position;
+        //save.num = PlayerPrefs.GetInt("Num");
+        //save.index = PlayerPrefs.GetInt("Index_");
 
 
 
 
-        for (int i = 0; i < save.num; i++)
-        {
+        //for (int i = 0; i < save.num; i++)
+        //{
 
-            GameObject zombies = GameObject.Find(PlayerPrefs.GetString("Name_zombies" + i));
-            GameObject.Find(PlayerPrefs.GetString("Name_zombies" + i)).transform.position = new Vector3(PlayerPrefs.GetFloat("zombie_x" + i), PlayerPrefs.GetFloat("zombie_y" + i), PlayerPrefs.GetFloat("zombie_z" + i));
-            //GameObject.Find(PlayerPrefs.GetString("Name_zombies" + i)).transform.rotation = Quaternion.Euler(zombies.transform.rotation.x -75, zombies.transform.rotation.y+180, zombies.transform.rotation.z);
+        //    GameObject zombies = GameObject.Find(PlayerPrefs.GetString("Name_zombies" + i));
+        //    GameObject.Find(PlayerPrefs.GetString("Name_zombies" + i)).transform.position = new Vector3(PlayerPrefs.GetFloat("zombie_x" + i), PlayerPrefs.GetFloat("zombie_y" + i), PlayerPrefs.GetFloat("zombie_z" + i));
+        //    //GameObject.Find(PlayerPrefs.GetString("Name_zombies" + i)).transform.rotation = Quaternion.Euler(zombies.transform.rotation.x -75, zombies.transform.rotation.y+180, zombies.transform.rotation.z);
             
-            Zombie_dead = GameObject.Find(PlayerPrefs.GetString("Name_zombies" + i)).GetComponent<Animator>();
+        //    Zombie_dead = GameObject.Find(PlayerPrefs.GetString("Name_zombies" + i)).GetComponent<Animator>();
 
-            Zombie_dead.SetBool("Dead_load", true);
+        //    Zombie_dead.SetBool("Dead_load", true);
 
-            //zombies.SetActive(false);
+        //    //zombies.SetActive(false);
 
-        }
+        //}
     }
-    public void Load_met()
-    {
-        Transform Cur_play_position = this.gameObject.transform;
-        Vector3 Player_Position = new Vector3(PlayerPrefs.GetFloat("PosX"), PlayerPrefs.GetFloat("PosY"), PlayerPrefs.GetFloat("PosZ"));
-        bulletsScipt.BulletsInClip = PlayerPrefs.GetInt("BulletsInClip");
-        bulletsScipt.BulletsInClip = PlayerPrefs.GetInt("BulletsInClip");
-        Cur_play_position.position = Player_Position;
-        save.num = PlayerPrefs.GetInt("Num");
-        save.index = PlayerPrefs.GetInt("Index");
-        for (int i = 0; i == save.num; i++)
-        {
-            GameObject zombies = GameObject.Find(PlayerPrefs.GetString("Name_zombies" + i));
-            zombies.SetActive(false);
+    //public void Load_met()
+    //{
+    //    Transform Cur_play_position = this.gameObject.transform;
+    //    Vector3 Player_Position = new Vector3(PlayerPrefs.GetFloat("PosX"), PlayerPrefs.GetFloat("PosY"), PlayerPrefs.GetFloat("PosZ"));
+    //    bulletsScipt.BulletsInClip = PlayerPrefs.GetInt("BulletsInClip");
+    //    bulletsScipt.BulletsInClip = PlayerPrefs.GetInt("BulletsInClip");
+    //    Cur_play_position.position = Player_Position;
+    //    save.num = PlayerPrefs.GetInt("Num");
+    //    save.index = PlayerPrefs.GetInt("Index");
+    //    for (int i = 0; i == save.num; i++)
+    //    {
+    //        GameObject zombies = GameObject.Find(PlayerPrefs.GetString("Name_zombies" + i));
+    //        zombies.SetActive(false);
 
-        }
-        // GameObject.Find("Zombie").SetActive(false);
-    }
+    //    }
+    //    // GameObject.Find("Zombie").SetActive(false);
+    //}
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.G))
         {
 
-            Load_met();
+            //Load_met();
 
 
 
